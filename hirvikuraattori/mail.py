ecf2519ac0a15c384cfec83e7ac1eacde1cf7a36
Jussi Tikkanen <drjustigious@gmail.com>
2022-01-03 21:20:43 +0200

    Add type hints to mail processing code

--- hirvikuraattori/mail.py
+++ hirvikuraattori/mail.py
@@ -6,7 +6,7 @@ import imaplib
 import json
 import os
 import time
-from typing import List, Dict, Set
+from typing import List, Dict, Set, ByteString, Tuple
 
 from hirvikuraattori import settings
 from hirvikuraattori.logs import logger
@@ -33,7 +33,7 @@ def write_tracker_json(previously_processed_emails: List[Dict], new_processed_em
         json.dump(processed_emails, tracker_file, ensure_ascii=False, indent=2)
 
 
-def get_processed_email_ids() -> Set[str]:
+def get_processed_email_ids() -> Tuple[Set[str], List[Dict]]:
     try:
         previously_processed_emails = load_tracker_json()
         processed_ids = set(
@@ -43,7 +43,7 @@ def get_processed_email_ids() -> Set[str]:
         return set(), []
 
 
-def get_unprocessed_email_ids(data) -> Set[str]:
+def get_unprocessed_email_ids(data: ByteString) -> Set[str]:
     all_ids = split_data_to_ids(data)
     processed_ids, previously_processed_emails = get_processed_email_ids()
     unprocessed_ids = all_ids - processed_ids
@@ -51,7 +51,7 @@ def get_unprocessed_email_ids(data) -> Set[str]:
     return unprocessed_ids, previously_processed_emails
 
 
-def split_data_to_ids(data) -> Set[str]:
+def split_data_to_ids(data: ByteString) -> Set[str]:
     ids = set()
     for id_string in data.decode("utf-8").split(" "):
         try:
@@ -62,14 +62,14 @@ def split_data_to_ids(data) -> Set[str]:
     return ids
 
 
-def get_email_message(email_data) -> Message:
+def get_email_message(email_data: ByteString) -> Message:
     email_message = email.message_from_bytes(
         email_data[0][1]
     )
     return email_message
 
 
-def get_email_message_dict(email_message, email_id):
+def get_email_message_dict(email_message: Message, email_id: str):
     message_dict = {"id": int(email_id)}
 
     for key in settings.STORED_EMAIL_METADATA:
@@ -83,7 +83,7 @@ def get_email_message_dict(email_message, email_id):
     return message_dict
 
 
-def attachment_is_downloadable(part):
+def attachment_is_downloadable(part: Message) -> bool:
     attachment_filename = part.get_filename()
     _, extension = os.path.splitext(attachment_filename)
 
@@ -96,7 +96,7 @@ def attachment_is_downloadable(part):
     return True
 
 
-def download_attachment(part, message_dict):
+def download_attachment(part: Message, message_dict: Dict) -> None:
     attachment_filename = part.get_filename()
     with open(attachment_filename, "wb") as downloaded_file:
         downloaded_file.write(part.get_payload(decode=True))
@@ -131,7 +131,7 @@ def process_email(mail: imaplib.IMAP4_SSL, email_id: str) -> Dict:
     return message_dict
 
 
-def process_emails(mail, email_ids):
+def process_emails(mail: imaplib.IMAP4_SSL, email_ids: str) -> List[Dict]:
     new_processed_emails = []
     for email_id in email_ids:
         time.sleep(settings.SLEEP_TIME_SECONDS_BETWEEN_EMAILS)
